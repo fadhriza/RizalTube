f2d08a3bb4b1a98a795c149dc4a41810c13107a6
Muhammad Rizal Fahilah <90969187+fadhriza@users.noreply.github.com>
2023-12-25 16:04:29 +0700

    Add files via upload

---
+++ RizalTube/stemplot.py
@@ -0,0 +1,12 @@
+
+# importing the module 
+#Install : pip3 install stemgraphic
+
+
+import stemgraphic 
+  
+data = [2316, 1884, 4769, 7049] 
+  
+# calling stem_graphic with required parameters, 
+# data and scale 
+stemgraphic.stem_graphic(data, scale = 4) 
